5f3031362a834dce5ca9980e8a42badf034af382
Minu James <minujames@Minus-MacBook-Pro.local>
2017-10-24 13:59:46 -0700

    adding html routes

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+// var path = require("path");
+
+var htmlRoutes = require("./routes/htmlRoutes.js")
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+htmlRoutes(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
